66a525c858f7ad314edf55b252c281709d729bbf
u818r69 <erip.rabbit4@gmail.com>
2023-01-08 14:17:01 +0900

    構文修正

--- basis/Question12/Question12.java
+++ basis/Question12/Question12.java
@@ -3,6 +3,10 @@ package basis.Question12;
 import java.util.Scanner;
 
 public class Question12 {
+	//選択肢の定数化
+	static final int NAME_NUMBER = 1;
+	static final int NO_NAME_NUMBER = 2;
+
 	public static void main(String[] args) {
 		//メッセージ出力
 		System.out.println("1：氏名を入力する");
@@ -13,27 +17,27 @@ public class Question12 {
 			String strInput1 = scanner.nextLine();
 			//String型からint型に型変換
 			int intInput = Integer.parseInt(strInput1);
+			//ifのみ構文
+			if (!(intInput == NAME_NUMBER || intInput == NO_NAME_NUMBER)) {
+				System.out.println("1～2の範囲で入力してください");
+				return;
+			}
+			//インスタンス生成
+			Person person = new Person();
 			//if文分岐
-			switch (intInput) {
-				case 1:
-					//メッセージ出力
-					System.out.println("氏名を入力してください");
-					System.out.print("氏名：");
-					//キーボード入力
-					String strInput2 = scanner.nextLine();
-					//インスタンス生成
-					Person person1 = new Person(strInput2);
-					//getterメソッドの呼び出し
-					System.out.println("私の名前は " + person1.getName() + " です");
-					break;
-				case 2:
-					//インスタンス生成
-					Person person2 = new Person();
-					//メソッド呼び出し
-					System.out.println("私の名前は " + person2.getName() + " です");
-					break;
-				default:
-					System.out.println("1～2の範囲で入力してください");
+			if (intInput == NAME_NUMBER) {
+				//メッセージ出力
+				System.out.println("氏名を入力してください");
+				System.out.print("氏名：");
+				//キーボード入力
+				String strInput2 = scanner.nextLine();
+				//メソッド呼び出し
+				person.setName(strInput2);
+				//getterメソッドの呼び出し
+				System.out.println("私の名前は " + person.getName() + " です");
+			} else {
+				//メソッド呼び出し
+				System.out.println("私の名前は " + person.getName() + " です");
 			}
 		} catch (NumberFormatException e) {//例外処理
 			System.out.println("整数以外の値が入力されました");
--- basis/Question13/Question13.java
+++ basis/Question13/Question13.java
@@ -3,8 +3,12 @@ package basis.Question13;
 import java.util.Scanner;
 
 public class Question13 {
+	//選択肢の定数化
+	static final int SUM_SUBTRACTION = 1;
+	static final int SUBTRACTION_SUM = 2;
+
 	public static void main(String[] args) {
-		//メッセージ出力
+		//メッセージ出力o
 		System.out.println("1：足し算結果⇒引き算結果の順で出力する");
 		System.out.println("2：引き算結果⇒足し算結果の順で出力する");
 		System.out.print("番号を選択してください（1,2）：");
@@ -14,39 +18,35 @@ public class Question13 {
 			String bango = scanner.nextLine();
 			//String型からint型への変換
 			int num = Integer.parseInt(bango);
-			if (num == 1 || num == 2) {
-				//メッセージ出力
-				System.out.println("整数を入力してください");
-				System.out.print("1個目：");
-				//キーボード入力
-				String strInput1 = scanner.nextLine();
-				//String型からint型への変換
-				int intInput1 = Integer.parseInt(strInput1);
-				//メッセージ出力
-				System.out.print("2個目：");
-				//キーボード入力
-				String strInput2 = scanner.nextLine();
-				//String型からint型への変換
-				int intInput2 = Integer.parseInt(strInput2);
-				//switch文
-				switch (num) {
-					//1の場合
-					case 1:
-						//CalculateSuperクラスのインスタンス生成
-						CalculateSuper calcSuper = new CalculateSuper(intInput1, intInput2);
-						//flowメソッドの呼び出し
-						calcSuper.flow();
-						break;
-					//2の場合
-					case 2:
-						//CalculateChildクラスのインスタンス生成
-						CalculateChild calcChild = new CalculateChild(intInput1, intInput2);
-						//flowメソッドの呼び出し
-						calcChild.flow();
-						break;
-				}
-			} else {
+			//ifのみ構文
+			if (!(num == SUM_SUBTRACTION || num == SUBTRACTION_SUM)) {
 				System.out.println("1～2の範囲で入力してください");
+				return;
+			}
+			//メッセージ出力
+			System.out.println("整数を入力してください");
+			System.out.print("1個目：");
+			//キーボード入力
+			String strInput1 = scanner.nextLine();
+			//String型からint型への変換
+			int intInput1 = Integer.parseInt(strInput1);
+			//メッセージ出力
+			System.out.print("2個目：");
+			//キーボード入力
+			String strInput2 = scanner.nextLine();
+			//String型からint型への変換
+			int intInput2 = Integer.parseInt(strInput2);
+			//if文
+			if (num == SUM_SUBTRACTION) {
+				//CalculateSuperクラスのインスタンス生成
+				CalculateSuper calcSuper = new CalculateSuper(intInput1, intInput2);
+				//flowメソッドの呼び出し
+				calcSuper.flow();
+			} else {
+				//CalculateChildクラスのインスタンス生成
+				CalculateChild calcChild = new CalculateChild(intInput1, intInput2);
+				//flowメソッドの呼び出し
+				calcChild.flow();
 			}
 		} catch (NumberFormatException e) {//catchメソッド
 			//メッセージ出力
